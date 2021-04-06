0c45cc1afd7f90306e85e2f2d7a66b28b05a5602
Евгений <ParzivalYandexLyceum@users.noreply.github.com>
2021-04-06 23:38:44 +0300

    version 1.1

--- demo/Samurai.py
+++ demo/Samurai.py
@@ -1,11 +1,12 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from my_token import token
 import requests
 import json
 import re
 import random
 from keep_alive import keep_alive
+from datetime import date
 
 prefix = "."
 bot = commands.Bot(command_prefix=prefix, help_command=None)
@@ -77,12 +78,23 @@ def embed_dog(author):
 
 
 @bot.event
+async def on_member_join(member):
+    await bot.get_channel(783742051556655174).send(member.name, "присоединлся к серверу")
+
+
+@bot.event
+async def on_member_remove(member):
+    await bot.get_channel(783742051556655174).send(member.name, "покинул сервер")
+
+
+@bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
     await bot.change_presence(activity=discord.Activity(
         type=discord.ActivityType.listening,
         name=f"{prefix}help - INVINCIBLE WARRIORS")
     )
+    await check_birthday.start()
 
 
 @bot.event
@@ -133,7 +145,7 @@ async def my_help(ctx):
     embed.add_field(name="Команды",
                     value=f"""**{prefix}help** - вывод информации о боте.
 **{prefix}xo_rules** - вывод информации о крестиках-ноликах.
-**{prefix}xo <member1> <member2>** - начало игры игры в крестики-нолики с указанием игроков.
+**{prefix}bd_rules** - вывод информации о днях рождения. 
 __Все команды вводятся **латинскими буквами**__""",
                     inline=False)
     await ctx.send(embed=embed)
@@ -266,6 +278,106 @@ async def place_error(ctx, error):
         await ctx.send("Вы не указали позицию")
     elif isinstance(error, commands.BadArgument):
         await ctx.send("Вы ввели не число")
+
+
+@bot.command(name="test")
+async def test(ctx):
+    embed = discord.Embed(
+        title=f"С ДНЕМ РОЖДЕНИЯ WARRIOR <@>",
+        description=":sweat_drops: :sweat_drops: :sweat_drops:",
+        colour=discord.Colour.purple()
+    )
+    embed.add_field(name="Блять какой же ты все таки ахуенный(-ая)",
+                    value="Поздравляю тебя с этим заебубительным днем. Желаю тебе счастья блять, радости, вот этой всей вашей человечей хуйни. Но никогда не забывай об этом сервере, здесь тебе "
+                          "всегда будут рады.",
+                    inline=False)
+    await ctx.send(embed=embed)
+
+
+@tasks.loop(hours=12)
+async def check_birthday():
+    today = str(date.today())[5:]
+    table = []
+    with open("birthdays.json", "r") as input_file:
+        data = json.load(input_file)
+        for i in range(len(data["bd"])):
+            info = list(data["bd"][i].keys())[0]
+            month, day = data["bd"][i][info]["month"], data["bd"][i][info]["day"]
+            current_date = date(2021, month, day).isoformat()[5:]
+            table.append([current_date, info])
+    for i in table:
+        if i[0] == today and i[1] != "test":
+            embed = discord.Embed(
+                title=f"С ДНЕМ РОЖДЕНИЯ WARRIOR <@{i[1]}>",
+                description=":sweat_drops: :sweat_drops: :sweat_drops:",
+                colour=discord.Colour.purple()
+            )
+            embed.add_field(name="Блять какой же ты все таки ахуенный(-ая)",
+                            value="Поздравляю тебя с этим заебубительным днем. Желаю тебе счастья блять, радости, вот этой всей вашей человечей хуйни. Но никогда не забывай об этом сервере, "
+                                  "здесь тебе всегда будут рады.",
+                            inline=False)
+            await bot.get_channel(778544220054224906).send(embed=embed)
+
+
+@bot.command(name="bd_rules")
+async def rules_birthday(ctx):
+    embed = discord.Embed(
+        title="Информация о календаре дней рождений",
+        description=":fire: :fire: :fire:",
+        colour=discord.Colour.purple()
+    )
+    embed.add_field(name="Команды",
+                    value=f"""**{prefix}bd <month> <day>** - внос в базу данных с указанием даты (можно внести только один раз:exclamation::exclamation::exclamation:)
+    **{prefix}bd_show** - выводит график всех дней рождений.
+__Все команды вводятся **латинскими буквами**__""",
+                    inline=False)
+    await ctx.send(embed=embed)
+
+
+@bot.command(name="bd_show")
+async def show_birthday_table(ctx):
+    embed = discord.Embed(
+        title="ДНИ РОЖДЕНИЯ",
+        colour=discord.Colour.purple()
+    )
+    with open("birthdays.json", "r") as file:
+        data = json.load(file)
+        for person in range(len(data["bd"])):
+            if not person:
+                continue
+            info = list(data["bd"][person].keys())[0]
+            month, day = data["bd"][person][info]["month"], data["bd"][person][info]["day"]
+            embed.add_field(name="<@{}>".format(int(info)),
+                            value=f"{month}.{day}",
+                            inline=False)
+        await ctx.send(embed=embed)
+
+
+@bot.command(name="bd")
+async def add_birthday_date(ctx, month: int, day: int):
+    if not any(i.id == 778542601690284034 for i in ctx.author.roles):
+        await ctx.send("У вас недостачно высокая роль для этой команды")
+        return
+    with open("birthdays.json") as info:
+        birthdays = json.load(info)
+        if any(str(ctx.author.id) in i.keys() for i in birthdays["bd"]):
+            await ctx.send("Вы уже указали свою дату")
+            return
+        if not (1 <= month <= 12 or 1 <= day <= 31):
+            await ctx.send("Вы указали неверную дату")
+            return
+        birthdays["bd"].append({ctx.author.id: {"month": month, "day": day}})
+        with open("birthdays.json", "w") as outfile:
+            json.dump(birthdays, outfile)
+        await ctx.send("Данные успешно внесены")
+
+
+@add_birthday_date.error
+async def add_birthday_date_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Вы указали неполную дату")
+    if isinstance(error, commands.BadArgument):
+        await ctx.send("Вы указали неверную дату")
 
 
 keep_alive()
